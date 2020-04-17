6c448ce3604f852b5167cac2aa672c27b07bd357
lubhani Agarwal <44601972+lubhaniagarwal@users.noreply.github.com>
2020-04-17 20:18:56 +0530

    Update twitterscraper.py

--- Twitterscraper/twitterscraper.py
+++ Twitterscraper/twitterscraper.py
@@ -1,5 +1,4 @@
 
 
 twitterscraper '#amazonlocker' --limit=5 --output=Twitterscraper\twitter.csv --csv
-
 twitterscraper (%23amazonlocker)%20lang%3Aen%20until%3A2020-04-17%20since%3A2019-01-01 --output=Twitterscraper\twitter2.csv --csv
